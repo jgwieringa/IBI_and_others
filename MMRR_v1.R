137f8709bbcb67946a23b4036255605d6879cb12
Jamin G Wieringa <31280625+jgwieringa@users.noreply.github.com>
2019-12-16 09:51:12 -0500

    Update MMRR_v1.R

--- MMRR_v1.R
+++ MMRR_v1.R
@@ -54,6 +54,21 @@ folders=list.dirs(path='.')
   ibi=dist(d2)
   ibi2=dist(d2,upper=TRUE,diag=TRUE)
   
+  #####Cost distance calculation
+  c1[c1<0]=0
+  d1[d1<0]=0
+  f1=SpatialPoints(b2)
+
+  tr_try <- transition(d1, transitionFunction=mean, directions=8)
+  tr_try2 <- transition(c1, transitionFunction=mean, directions=8)
+  m=as.matrix(ibi2)
+  m_a=as.matrix(ibe2)
+  f2=costDistance(tr_try,fromCoords = f1, toCoords = f1)
+  f2_a=costDistance(tr_try2,fromCoords = f1, toCoords = f1)
+
+  ibe_a=as.dist(f2_a)
+  ibi_a=as.dist(f2)
+
   
   ###MRM function
   ##Create output
@@ -62,10 +77,12 @@ folders=list.dirs(path='.')
   ##Put species in file
   print(files1)
   ##Create MMRR output
-  print(MRM(formula = gen_mat~ibd+ibe+ibi))
+  print(MRM(formula = gen_mat~ibd+ibe+ibi_a))
+
+  print(cor(ibd,ibe))
+  print(cor(ibd,ibi_a))
+  print(cor(ibe,ibi_a))
 
   ##Close output
   sink()
   
-  print(MRM(formula = gen_mat~ibd+ibe))
-  print(MRM(formula = gen_mat~ibd+ibi))
